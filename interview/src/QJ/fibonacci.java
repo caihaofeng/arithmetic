8ca859bf6bb3036a72b97144abcbe1c0b0fb4ed5
fengback <734948551@qq.com>
2019-03-11 22:31:52 +0800

    增加了fibonacci函数，使用递归实现的

---
+++ interview/src/QJ/fibonacci.java
@@ -0,0 +1,22 @@
+package QJ;
+
+public class fibonacci {
+
+	
+	public static  int fibonacci(int n) {
+		
+		if(n==1||n==2) {
+			return 1;
+		}
+		else {
+		return  fibonacci(n-1)+fibonacci(n-2);
+		
+		}
+	}
+	public static void main(String[] args) {
+		int n=7;
+		fibonacci.fibonacci(n);
+		System.out.println(fibonacci(n));
+	}
+}
+
--- interview/src/SF/MergeSort.java
+++ interview/src/SF/MergeSort.java
@@ -9,42 +9,38 @@ public class MergeSort {
 	}
 	private static void merge(int [] arr,int l,int mid ,int r) {
 		
-		int [] aux =Arrays.copyOfRange(arr, l, r);
+		int [] aux =Arrays.copyOfRange(arr, l, r+1);
 		
-		int i=l;
-		int j=mid+1;
-		for (int k=l;k<=r;k++) {
-			if(i>mid) {
-				aux[k]=aux[j];
-				j++;
-			}
-			else if(j>r) {
-				aux[k]=aux[i];
-				i++;
-			}
-			
-			else if(aux[i-l]<aux[j]) {
-			
-				aux [k]=aux[i-1];
-				i++;
-			}
-			else 
-			{
-				aux[k]=aux[j-1];
-				j++;
-			}
+		int i = l, j = mid+1;
+		for (int k = l ; k <= r; k ++ ) {
+			if( i > mid ){  // 如果左半部分元素已经全部处理完毕
+                arr[k] = aux[j]; j ++;
+            }
+            else if( j > r ){   // 如果右半部分元素已经全部处理完毕
+                arr[k] = aux[i-l]; i ++;
+            }
+            else if( aux[i-l] < aux[j-l] ){  // 左半部分所指元素 < 右半部分所指元素
+                arr[k] = aux[i-l]; i ++;
+            }
+            else{  // 左半部分所指元素 >= 右半部分所指元素
+                arr[k] = aux[j-l]; j ++;
+            }
 		}
 	}
 	
 	private static void sort(int [] arr,int l, int r) {
 		 System.out.println("in mergesort, l =" + l + ", r =" + r );
-		if(l>r)
-			return ;
-		int mid =(l+r)/2;
-		sort(arr,l,mid);
+		if(l>=r)
+			return ;	
+		 System.out.println("in mergesort, l1 =" + l + ", r1 =" + r );
+    	int mid =(l+r)/2;
+    	 System.out.println("int mid =(l+r)/2, l2 =" + l + ", r2 =" + r + ", mid= "+mid);
+		sort(arr,l,mid);	
+		 System.out.println("isort(arr,l,mid), l3 =" + l + ", r3 =" + r + ", mid= "+mid );
 		sort(arr,mid+1,r);
-		merge(arr,l,r, mid);
-		
+		 System.out.println("sort(arr,mid+1,r), l4 =" + l + ", r4 =" + r + ", mid= "+mid );
+		merge(arr,l, mid,r);
+		 System.out.println("merge(arr,l, mid,r), l5 =" + l + ", r5 =" + r + ", mid= "+mid );
 	}
 	
 	
@@ -53,11 +49,11 @@ public class MergeSort {
 		sort(arr, 0, n-1);
 	}
 	public static void main(String[] args) {
-		int [] arr= {9,8,7,6,5,4,3,2,1};
-		InsertSort.sort(arr);
+		int [] arr= {5,4,3,2,1};
+		MergeSort.sort(arr);
 		for (int i = 0; i < arr.length; i++) {
 			System.out.print(arr[i]);;
-			System.out.print("");
+			System.out.print(" ");
 		}
 	}
 }
